f6e3e8b478733f26c0d6cc3322e656f9d8d6b423
Billy Everyteen <jamesanelay@gmail.com>
2014-09-04 22:42:34 +0100

    Add license info

--- src/app/code/community/Theextensionlab/Trustpilot/Block/Feed.php
+++ src/app/code/community/Theextensionlab/Trustpilot/Block/Feed.php
@@ -1,5 +1,18 @@
-<?php class Theextensionlab_Trustpilot_Block_Feed extends Mage_Core_Block_Template
+
+<?php
+/**
+ * The Extension Lab
+ *
+ * @category    Theextensionlab
+ * @package     Theextensionlab_Trustpilot
+ * @copyright   Copyright (c) 2014 The Extension Lab (http://www.theextensionlab.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author      James Anelay <jamesanelay@theextensionlab.com>
+ */
+
+class Theextensionlab_Trustpilot_Block_Feed extends Mage_Core_Block_Template
 {
+
     public function __construct(){
 
     }
--- src/app/code/community/Theextensionlab/Trustpilot/Helper/Data.php
+++ src/app/code/community/Theextensionlab/Trustpilot/Helper/Data.php
@@ -1,4 +1,16 @@
-<?php class Theextensionlab_Trustpilot_Helper_Data extends Mage_Core_Helper_Abstract
+<?php
+/**
+ * The Extension Lab
+ *
+ * @category    Theextensionlab
+ * @package     Theextensionlab_Trustpilot
+ * @copyright   Copyright (c) 2014 The Extension Lab (http://www.theextensionlab.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author      James Anelay <jamesanelay@theextensionlab.com>
+ */
+
+
+class Theextensionlab_Trustpilot_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
 }
